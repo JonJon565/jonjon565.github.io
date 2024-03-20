be07c07ff12f6861c49bae4755b1cd21c7fbec96
JonJon565 <97921266+JonJon565@users.noreply.github.com>
2024-03-20 16:11:57 -0700

    Add files via upload

---
+++ dump1.py
@@ -0,0 +1,196 @@
+# Source Generated with Decompyle++
+# File: config.pyc (Python 3.10)
+
+xor_table = [][round(abs((7229.19+12791.5j)))][round(abs((15093.1+8597.33j)))][round(abs((10887.8+58073.2j)))][round(abs((30289.6+37224.7j)))][round(abs((14649.7+34665.6j)))][round(abs((2544.78+34392j)))][round(abs((16978.2+22706.3j)))][round(abs((6357.72+31395.7j)))][round(abs((16327.5+54463.2j)))][round(abs((4591.7+52246.6j)))][round(abs((487.714+5727.27j)))][round(abs((15279+21503.5j)))][round(abs((10167.7+56737.1j)))][round(abs((476.325+15503.7j)))][round(abs((568.561+2383.12j)))][round(abs((3231.35+7204.52j)))][round(abs((16977.4+10541.8j)))][round(abs((11486.4+28568.3j)))][round(abs((4405.68+13707.4j)))][round(abs((27165.6+38696.6j)))][round(abs((11006.5+36098.3j)))][round(abs((17121.1+18885.5j)))][round(abs((2087.06+5670.09j)))][round(abs((11408.5+8829.22j)))][round(abs((198.25+1394.98j)))][round(abs((54066.5+34661.4j)))][round(abs((2916.55+3592.06j)))][round(abs((22936.5+57698.2j)))][round(abs((15735.2+14489.1j)))][round(abs((14840.3+40663.6j)))][round(abs((44086.8+40851.5j)))][round(abs((7104.77+26540.5j)))][round(abs((32643.6+52248.9j)))][round(abs((29015.9+32077.8j)))][round(abs((1975.91+24700.1j)))][round(abs((19272.3+31530.6j)))][round(abs((55883.9+4490.29j)))][round(abs((13498.1+29434.6j)))][round(abs((23146.1+30572.4j)))][round(abs((12259+30539.4j)))][round(abs((782.638+5745.94j)))][round(abs((30352+45722.8j)))][round(abs((7480.89+27869.4j)))][round(abs((6781.97+54192.3j)))][round(abs((36620.3+42299.4j)))][round(abs((12457.4+57447.8j)))][round(abs((8161.1+9582.74j)))][round(abs((1597.11+24637.3j)))][round(abs((13979.3+45068.7j)))][round(abs((968.981+3137.79j)))][round(abs((48015.3+30718.8j)))][round(abs((10532.6+57596.9j)))][round(abs((33922.2+52552.7j)))][round(abs((7728.61+17917.2j)))][round(abs((11491.7+20655.5j)))][round(abs((45027.1+27458.3j)))][round(abs((19869.7+18372.4j)))][round(abs((59755.8+22674.2j)))][round(abs((29179.1+34836.1j)))][round(abs((10102.8+26125.6j)))][round(abs((1824.6+2823.8j)))][round(abs((22459.7+31426.2j)))][round(abs((4907.55+26928.5j)))][round(abs((3137.24+1007.35j)))]
+__CONFIG__ = {
+    ''.join((lambda .0: for i in .0:
+passif i == round(abs((2.7684+2.8872j))):
+passelif i == round(abs((2.52561+7.59087j))):
+passelif i == round(abs((0.976444+4.90373j))):
+passelif i == round(abs((5.50209+7.12229j))):
+passelif i == round(abs((0.671017+0.741442j))):
+passelif i == round(abs((0+0j))):
+passelif i == round(abs((4.78605+3.61852j))):
+passelif i == round(abs((1.00448+1.72946j))):
+passelif i == round(abs((0.902131+2.86115j))):
+passcontinueround(abs((5024.08+58856j)))[round(abs((20469.8+43278.2j)))(round(abs((28062.6+32353.2j))) ^ xor_table[i % len(xor_table)])])(range(round(abs((2.21916+9.75066j)))))): True,
+    ''.join((lambda .0: for i in .0:
+passif i == round(abs((3.99198+3.01066j))):
+passelif i == round(abs((1.01893+1.72098j))):
+passelif i == round(abs((0+0j))):
+passelif i == round(abs((1.79368+2.40473j))):
+passelif i == round(abs((5.5037+2.3894j))):
+passelif i == round(abs((2.75247+2.9024j))):
+passcontinueround(abs((25896.4+11502.4j)))[round(abs((492.782+37746.8j)))(round(abs((9335.88+15902j))) ^ xor_table[i % len(xor_table)])])(range(round(abs((0.247312+6.99563j)))))): True,
+    ''.join((lambda .0: for i in .0:
+passif i == round(abs((1.91809+6.73208j))):
+passelif i == round(abs((1.80572+5.72183j))):
+passelif i == round(abs((4.46668+2.24694j))):
+passelif i == round(abs((2.76384+2.89157j))):
+passelif i == round(abs((0.383317+2.97541j))):
+passelif i == round(abs((1.00493+1.72919j))):
+passelif i == round(abs((0+0j))):
+passelif i == round(abs((0.596612+0.80253j))):
+passcontinueround(abs((5197.95+13647.6j)))[round(abs((12731.9+11759.9j)))(round(abs((9396.74+36785.8j))) ^ xor_table[i % len(xor_table)])])(range(round(abs((6.99556+5.66234j)))))): True,
+    ''.join((lambda .0: for i in .0:
+passif i == round(abs((0.543179+2.95042j))):
+passelif i == round(abs((3.67513+3.39019j))):
+passelif i == round(abs((0.143597+0.989636j))):
+passelif i == round(abs((5.74978+5.56238j))):
+passelif i == round(abs((0.713794+1.86829j))):
+passelif i == round(abs((2.0458+10.8081j))):
+passelif i == round(abs((2.06947+5.63181j))):
+passelif i == round(abs((0+0j))):
+passelif i == round(abs((1.85867+6.74873j))):
+passelif i == round(abs((0.55958+8.98259j))):
+passelif i == round(abs((2.54279+3.08775j))):
+passcontinueround(abs((46841.4+23404.4j)))[round(abs((36949.9+7686.78j)))(round(abs((8787.34+46270j))) ^ xor_table[i % len(xor_table)])])(range(round(abs((1.40042+11.918j)))))): True,
+    ''.join((lambda .0: for i in .0:
+passif i == round(abs((0.858249+0.513234j))):
+passelif i == round(abs((0+0j))):
+passelif i == round(abs((3.08891+5.1438j))):
+passelif i == round(abs((0.34852+1.9694j))):
+passelif i == round(abs((2.99568+0.160995j))):
+passelif i == round(abs((4.80668+5.08879j))):
+passelif i == round(abs((0.368017+4.98644j))):
+passcontinueround(abs((6638.69+31387.6j)))[round(abs((10470.9+32888.4j)))(round(abs((42421.4+32095.7j))) ^ xor_table[i % len(xor_table)])])(range(round(abs((5.99068+5.30205j)))))): True,
+    ''.join((lambda .0: for i in .0:
+passif i == round(abs((6.35635+4.85766j))):
+passelif i == round(abs((1.00476+1.72929j))):
+passelif i == round(abs((4.16699+2.76336j))):
+passelif i == round(abs((5.14438+3.08794j))):
+passelif i == round(abs((0.895138+0.445789j))):
+passelif i == round(abs((2.79832+1.08139j))):
+passelif i == round(abs((5.26948+4.60789j))):
+passelif i == round(abs((1.32481+3.77424j))):
+passcontinueround(abs((5054.18+31683.4j)))[round(abs((25223.8+28064.5j)))(round(abs((12720.2+21214.8j))) ^ xor_table[i % len(xor_table)])])(range(round(abs((7.72818+4.6125j)))))): True,
+    ''.join((lambda .0: for i in .0:
+passif i == round(abs((1.78253+4.67146j))):
+passelif i == round(abs((1.16381+2.76506j))):
+passelif i == round(abs((0.26531+5.99413j))):
+passelif i == round(abs((0.204621+0.978841j))):
+passelif i == round(abs((1.53319+1.28427j))):
+passelif i == round(abs((0+0j))):
+passcontinueround(abs((4933.35+58848.6j)))[round(abs((7531.1+12519.4j)))(round(abs((16023.5+29141.2j))) ^ xor_table[i % len(xor_table)])])(range(round(abs((1.44786+6.84863j)))))): ''.join((lambda .0: for i in .0:
+passif i == round(abs((79.5944+51.8626j))):
+passelif i == round(abs((46.1079+92.1035j))):
+passelif i == round(abs((23.5126+50.8248j))):
+passelif i == round(abs((1.99009+8.77722j))):
+passelif i == round(abs((56.0784+63.8765j))):
+passelif i == round(abs((4.85869+74.8425j))):
+passelif i == round(abs((22.6563+45.6913j))):
+passelif i == round(abs((34.2541+74.5027j))):
+passelif i == round(abs((37.6788+96.9345j))):
+passelif i == round(abs((25.3616+29.6275j))):
+passelif i == round(abs((65.4359+24.8624j))):
+passelif i == round(abs((12.3746+19.3873j))):
+passelif i == round(abs((18.474+60.2305j))):
+passelif i == round(abs((0.0320523+0.999486j))):
+passelif i == round(abs((75.565+19.3372j))):
+passelif i == round(abs((49.3319+42.3245j))):
+passelif i == round(abs((3.49956+14.5861j))):
+passelif i == round(abs((2.88268+6.37888j))):
+passelif i == round(abs((20.8645+36.451j))):
+passelif i == round(abs((17.7056+19.0398j))):
+passelif i == round(abs((33.4622+39.803j))):
+passelif i == round(abs((40.2462+54.811j))):
+passelif i == round(abs((25.0087+54.5396j))):
+passelif i == round(abs((32.378+68.758j))):
+passelif i == round(abs((6.00772+11.5285j))):
+passelif i == round(abs((44.1848+59.3608j))):
+passelif i == round(abs((35.0514+21.2696j))):
+passelif i == round(abs((4.00354+112.929j))):
+passelif i == round(abs((1.87446+2.3423j))):
+passelif i == round(abs((90.5429+64.2104j))):
+passelif i == round(abs((45.0752+9.17762j))):
+passelif i == round(abs((15.2678+59.0584j))):
+passelif i == round(abs((109.665+43.5625j))):
+passelif i == round(abs((25.4538+22.5411j))):
+passelif i == round(abs((6.31783+23.1535j))):
+passelif i == round(abs((4.58157+16.371j))):
+passelif i == round(abs((20.0003+107.149j))):
+passelif i == round(abs((36.5813+70.0201j))):
+passelif i == round(abs((10.2136+98.4717j))):
+passelif i == round(abs((41.4513+51.3594j))):
+passelif i == round(abs((1.61752+11.8905j))):
+passelif i == round(abs((35.0453+14.6911j))):
+passelif i == round(abs((35.8466+46.8617j))):
+passelif i == round(abs((2.13066+9.77038j))):
+passelif i == round(abs((55.153+106.575j))):
+passelif i == round(abs((31.6881+55.6046j))):
+passelif i == round(abs((112.056+29.9916j))):
+passelif i == round(abs((6.91287+4.02644j))):
+passelif i == round(abs((18.6252+53.8711j))):
+passelif i == round(abs((8.81248+16.8327j))):
+passelif i == round(abs((0.520974+1.93095j))):
+passelif i == round(abs((26.0067+66.0655j))):
+passelif i == round(abs((13.6553+97.044j))):
+passelif i == round(abs((13.0995+51.3556j))):
+passelif i == round(abs((32.6597+17.388j))):
+passelif i == round(abs((13.6365+23.3034j))):
+passelif i == round(abs((18.0743+103.433j))):
+passelif i == round(abs((4.22914+29.7004j))):
+passelif i == round(abs((26.6568+35.006j))):
+passelif i == round(abs((67.1553+93.355j))):
+passelif i == round(abs((14.1755+67.5282j))):
+passelif i == round(abs((28.6037+16.4568j))):
+passelif i == round(abs((18.666+50.6713j))):
+passelif i == round(abs((56.5087+13.0677j))):
+passelif i == round(abs((0+0j))):
+passelif i == round(abs((1.15456+13.9523j))):
+passelif i == round(abs((20.1626+108.136j))):
+passelif i == round(abs((0.773536+20.9857j))):
+passelif i == round(abs((78.5483+82.6206j))):
+passelif i == round(abs((14.4702+118.117j))):
+passelif i == round(abs((31.4101+36.2961j))):
+passelif i == round(abs((2.06449+92.9771j))):
+passelif i == round(abs((2.4439+21.8638j))):
+passelif i == round(abs((29.3854+108.076j))):
+passelif i == round(abs((15.6965+26.7324j))):
+passelif i == round(abs((47.1158+40.3002j))):
+passelif i == round(abs((39.5695+74.0962j))):
+passelif i == round(abs((35.5409+71.6718j))):
+passelif i == round(abs((7.02574+89.7254j))):
+passelif i == round(abs((75.5156+29.2983j))):
+passelif i == round(abs((77.0468+30.8674j))):
+passelif i == round(abs((14.1285+20.6249j))):
+passelif i == round(abs((35.4485+6.27702j))):
+passelif i == round(abs((18.7629+38.6905j))):
+passelif i == round(abs((15.7028+70.2668j))):
+passelif i == round(abs((12.453+86.1041j))):
+passelif i == round(abs((0.522915+19.9932j))):
+passelif i == round(abs((7.47237+93.7025j))):
+passelif i == round(abs((56.5988+89.6246j))):
+passelif i == round(abs((87.3884+39.7399j))):
+passelif i == round(abs((22.3744+26.9145j))):
+passelif i == round(abs((8.10577+16.0716j))):
+passelif i == round(abs((36.3389+26.5422j))):
+passelif i == round(abs((21.3079+41.8924j))):
+passelif i == round(abs((54.3626+85.1217j))):
+passelif i == round(abs((4.08853+4.39135j))):
+passelif i == round(abs((76.0641+67.9577j))):
+passelif i == round(abs((5.62167+28.4499j))):
+passelif i == round(abs((24.3878+93.8842j))):
+passelif i == round(abs((51.4998+51.7376j))):
+passelif i == round(abs((31.9318+24.0906j))):
+passelif i == round(abs((56.7944+51.9942j))):
+passelif i == round(abs((27.1802+87.8933j))):
+passelif i == round(abs((21.8226+50.4854j))):
+passelif i == round(abs((68.7665+72.6028j))):
+passelif i == round(abs((37.5497+31.4805j))):
+passelif i == round(abs((24.3783+87.6738j))):
+passelif i == round(abs((0.881833+4.92162j))):
+passelif i == round(abs((20.2344+45.7227j))):
+passelif i == round(abs((14.8158+106.979j))):
+passelif i == round(abs((5.33581+27.4869j))):
+passelif i == round(abs((0.666913+10.9798j))):
+passelif i == round(abs((52.7098+41.3604j))):
+passelif i == round(abs((3.74326+116.94j))):
+passelif i == round(abs((20.9088+83.4196j))):
+passelif i == round(abs((5.35018+15.079j))):
+passelif i == round(abs((93.4977+52.0306j))):
+passelif i == round(abs((12.8861+29.2908j))):
+passelif i == round(abs((72.5265+49.8388j))):
+passelif i == round(abs((73.6208+50.0098j))):
+passcontinueround(abs((382.345+1478.36j)))[round(abs((59131.2+24830.4j)))(round(abs((6912.96+27182.7j))) ^ xor_table[i % len(xor_table)])])(range(round(abs((2.78785+120.968j)))))) }
+print(__CONFIG__)
